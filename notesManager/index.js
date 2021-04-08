7d7f05ba5f7bbdb258ffce969653ce3f0a6bfc32
acotovici <alexandra.cotovici@endava.com>
2021-04-08 21:51:01 +0300

    added POST method using callbacks and promisses

--- notesManager/index.js
+++ notesManager/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const util = require('util')
 
 const express = require('express')
 const app = express()
@@ -23,27 +24,15 @@ app.get('/notes', (req, res) => {
   })
 })
 
-app.get('/notes', (req, res) => {
-  const data = JSON.parse(fs.readFile('./data.json'))
-  const dataToAdd = { title: 'Title3', content: 'Content 3' }
-  let newContent = JSON.stringify([...data, { title, content }])
-  fs.writeFile('./data.json', newContent, { flag: 'a+' }, (err) => {
-    if (err) {
-      return console.log(error)
-    }
-    console.log('added some data to notes file', content)
-    res.send(content)
-  })
-})
-
 // find a note by title
 app.get('/notes/:title', (req, res) => {
+  const { title } = req.params
+
   const notesFromFile = fs.readFile('./data.json', 'utf8', (err, data) => {
     if (err) {
       return res.send(err.message)
     }
     const parsedNotes = JSON.parse(data)
-    const { title } = req.params
     const note = parsedNotes.find((note) => note.title === title)
     if (note) res.status(200).send(note)
     else res.status(400).send('Note not found')
@@ -52,15 +41,96 @@ app.get('/notes/:title', (req, res) => {
 
 // add data from req body
 app.post('/notes', (req, res) => {
-  const content = req.body
-  fs.writeFile('./data.json', content, { flag: 'a+' }, (err) => {
+  const { title, content } = req.body
+  fs.readFile('./data.json', 'utf8', (err, data) => {
     if (err) {
-      return console.log(error)
+      console.log(err)
+      return res.send(err.message)
     }
-    console.log('some data to notes file', content)
-    res.send(content)
+    const parsedNotes = JSON.parse(data)
+    let newContent = JSON.stringify([...parsedNotes, { title, content }])
+
+    fs.writeFile('./data.json', newContent, { flag: 'w' }, (err) => {
+      if (err) {
+        console.log(err)
+        return res.send(err.message)
+      }
+      console.log('some data to notes file', newContent)
+      res.send(newContent)
+      res.status(201).send('New note added!')
+    })
   })
-  res.status(201).send('New note added!')
+})
+
+app.post('/notes-promisify', (req, res) => {
+  const { title, content } = req.body
+  const fsReadFileAsync = util.promisify(fs.readFile)
+  const fsWriteFileAsync = util.promisify(fs.writeFile)
+
+  fsReadFileAsync('./data.json', 'utf8')
+    .then((data) => {
+      const parsedNotes = JSON.parse(data)
+      // throw new Error('New error !')
+      let newContent = JSON.stringify([...parsedNotes, { title, content }])
+      fsWriteFileAsync('./data.json', newContent, { flag: 'w' })
+        .then(() => {
+          res.send(newContent)
+          res.status(201).send('New note added!')
+        })
+        .catch((err) => {
+          console.log(err)
+          return res.send(err.message)
+        })
+    })
+    .catch((err) => {
+      console.log(err)
+      // mandatory to return res to user
+      return res.send(err.message)
+    })
+})
+
+app.post('/notes-promisify-2', (req, res) => {
+  const { title, content } = req.body
+  const fsReadFileAsync = (file, encoding) =>
+    new Promise((resolve, reject) => {
+      fs.readFile(file, encoding, (err, data) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(data)
+      })
+    })
+
+  const fsWriteFileAsync = (file, newContent, options) =>
+    new Promise((resolve, reject) => {
+      fs.writeFile(file, newContent, options, (err) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+
+  fsReadFileAsync('./data.json', 'utf8')
+    .then((data) => {
+      const parsedNotes = JSON.parse(data)
+      // throw new Error('New error !')
+      let newContent = JSON.stringify([...parsedNotes, { title, content }])
+      fsWriteFileAsync('./data.json', newContent, { flag: 'w' })
+        .then(() => {
+          res.send(newContent)
+          res.status(201).send('New note added!')
+        })
+        .catch((err) => {
+          console.log(err)
+          return res.send(err.message)
+        })
+    })
+    .catch((err) => {
+      console.log(err)
+      // mandatory to return res to user
+      return res.send(err.message)
+    })
 })
 
 app.listen(port, () => {
