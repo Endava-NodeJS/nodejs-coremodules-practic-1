56ecc57f7d5cc1fa3f709100ee9b700cded22e98
acotovici <alexandra.cotovici@endava.com>
2021-04-22 13:58:54 +0300

    added delete and edit notes by using delete and put methods

--- notesManager/index.js
+++ notesManager/index.js
@@ -24,21 +24,6 @@ app.get('/notes', (req, res) => {
   })
 })
 
-// find a note by title
-app.get('/notes/:title', (req, res) => {
-  const { title } = req.params
-
-  const notesFromFile = fs.readFile('./data.json', 'utf8', (err, data) => {
-    if (err) {
-      return res.send(err.message)
-    }
-    const parsedNotes = JSON.parse(data)
-    const note = parsedNotes.find((note) => note.title === title)
-    if (note) res.status(200).send(note)
-    else res.status(400).send('Note not found')
-  })
-})
-
 // add data from req body
 app.post('/notes', (req, res) => {
   const { title, content } = req.body
@@ -62,6 +47,7 @@ app.post('/notes', (req, res) => {
   })
 })
 
+// add note using util promisify
 app.post('/notes-promisify', (req, res) => {
   const { title, content } = req.body
   const fsReadFileAsync = util.promisify(fs.readFile)
@@ -89,6 +75,7 @@ app.post('/notes-promisify', (req, res) => {
     })
 })
 
+// add note using Promise
 app.post('/notes-promisify-2', (req, res) => {
   const { title, content } = req.body
   const fsReadFileAsync = (file, encoding) =>
@@ -131,6 +118,85 @@ app.post('/notes-promisify-2', (req, res) => {
       // mandatory to return res to user
       return res.send(err.message)
     })
+})
+
+// find a note by title
+app.get('/notes/:title', (req, res) => {
+  const { title } = req.params
+
+  const notesFromFile = fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+      return res.send(err.message)
+    }
+    const parsedNotes = JSON.parse(data)
+    const note = parsedNotes.find((note) => note.title === title)
+    if (note) res.status(200).send(note)
+    else res.status(400).send('Note not found')
+  })
+})
+
+// delete a note by id using DELETE method
+app.delete('/notes/delete/:id', (req, res) => {
+  const { id } = req.params
+
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+      return res.send(err.message)
+    }
+
+    const parsedNotes = JSON.parse(data)
+    const noteId = parsedNotes.find((note) => note.title === id)
+
+    if (noteId) {
+      const filteredData = parsedNotes.filter((value) => {
+        return value !== noteId
+      })
+
+      let newContent = JSON.stringify(filteredData)
+
+      fs.writeFile('./data1.json', newContent, (err) => {
+        if (err) {
+          console.log(err)
+          return res.send(err.message)
+        }
+      })
+      res.status(200).send(filteredData)
+    } else return res.status(400).send('Note not found')
+  })
+})
+
+// edit a note using PUT method
+
+app.put('/notes/update/:id', (req, res) => {
+  const { id } = req.params
+  const { title, content } = req.body
+
+  if (!title || !content) {
+    res.status(400).send('Provide data to update')
+  }
+
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+      return res.send(err.message)
+    }
+
+    const parsedNotes = JSON.parse(data)
+    const noteId = parsedNotes.find((note) => note.title === id)
+
+    if (noteId) {
+      let newContent = JSON.stringify([...parsedNotes, { title, content }])
+
+      fs.writeFile('./data2.json', newContent, (err) => {
+        if (err) {
+          console.log(err)
+          return res.send(err.message)
+        }
+        res.status(200).send(newContent)
+      })
+    } else return res.status(400).send('Note not found')
+  })
 })
 
 app.listen(port, () => {
